4f6a4eb3d4a1da3a1522d5e699cfdff3fa8a987e
toxicshadow <toxicshadow@PC_1.lan>
2018-07-25 13:52:22 +0100

    First submission for 32bit linux support
    Note: This compiles but seems to have problems with the latest GL code causing sigsev. Uploading so people with GL experience can investigate. There is still a chance that the dynarec will not work on this build and will require further investigation.

--- core/rec-x86/rec_x86_asm.cpp
+++ core/rec-x86/rec_x86_asm.cpp
@@ -4,10 +4,25 @@
 
 #include "rec_x86_ngen.h"
 
+extern const u32 cpurun_offset;
+
+unsigned int ngen_required = true;
+
 u32 gas_offs=offsetof(Sh4RCB,cntx.jdyn);
 
 const u32 cpurun_offset=offsetof(Sh4RCB,cntx.CpuRunning);
 
 void (*ngen_FailedToFindBlock)()=&ngen_FailedToFindBlock_;
+
+u32* GetRegPtr(u32 reg)
+{
+    return Sh4_int_GetRegisterPtr((Sh4RegType)reg);
+}
+
+void ngen_terminate(void)
+{
+   ngen_required = false;
+}
+
 #endif
 
